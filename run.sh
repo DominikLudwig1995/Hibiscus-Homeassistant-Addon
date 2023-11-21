9b6402194af83d189322e42df1a1b354e61942ec
root <root@a0d7b954-ssh.local.hass.io>
2023-11-21 12:20:22 +0100

    NOTICKET (fix): password parameter

--- run.sh
+++ run.sh
@@ -11,5 +11,5 @@ CONFIG_FILE="$BASE_PATH/de.willuhn.jameica.hbci.rmi.HBCIDBService.properties"
 
 sed -i -e "s|DATABASE_URL|${DATABASE_URL}|g" -e "s|DATABASE_USERNAME|${DATABASE_USERNAME}|g" -e "s|DATABASE_PASSWORD|${DATABASE_PASSWORD}|g" $CONFIG_FILE
 
-/home/hibiscus/hibiscus-server/jameicaserver.sh -w $HIBISCUS_PASSWORD -f /data
+/home/hibiscus/hibiscus-server/jameicaserver.sh -p $HIBISCUS_PASSWORD -f /data
 #tail -f /dev/null
